9f49aa0dc9a358d371352aa49eec791458ecc819
myfosse <andrey.egorov.minsk@gmail.com>
2021-05-15 12:57:52 +0300

    Initial commit

---
+++ src/main/java/by/bsu/oauth2/githubgoogle/GithubGoogleApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.oauth2.githubgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGoogleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubGoogleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsu/oauth2/githubgoogle/GithubGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.oauth2.githubgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGoogleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
