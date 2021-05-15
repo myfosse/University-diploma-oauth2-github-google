c7bf42ab7bb0d0a9bc3a5a30bc26b57fc3f08ea6
myfosse <andrey.egorov.minsk@gmail.com>
2021-05-15 13:54:20 +0300

    Add github oauth2

---
+++ src/main/java/by/bsu/oauth2/githubgoogle/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package by.bsu.oauth2.githubgoogle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/** @author Andrey Egorov */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(
+            a ->
+                a.antMatchers("/", "/error", "/webjars/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .logout(l -> l.logoutSuccessUrl("/").permitAll())
+        .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .exceptionHandling(
+            e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .oauth2Login();
+  }
+}
